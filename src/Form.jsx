311dffb2a33f810b3ed9bfed4b69c21a9611659e
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-01-06 23:05:23 +0530

    few changes in form

--- src/Form.jsx
+++ src/Form.jsx
@@ -18,12 +18,9 @@ const Form = () => {
         setIsFormOpen(false)
     }
     
-
-
-
   return (
-    <div className='border flex flex-col gap-5 rounded-2xl border-gray-400 bg-gray-800 w-full max-w-md '>
-        <div className='flex justify-between border-b p-4 items-center'>
+    <div className='border flex flex-col gap-5 rounded-2xl border-gray-400 bg-gray-800 w-full max-w-md text-white'>
+        <div className='flex justify-between border-b p-4 items-center '>
             <h1 className='text-xl font-bold'>New Event</h1>
             <X className='text-red-400 cursor-pointer' onClick={()=>setIsFormOpen(false)}/>
         </div>
@@ -35,21 +32,21 @@ const Form = () => {
                 </div>
                 <div className='flex gap-4'>
                     <label htmlFor="start">From</label>
-                    <input type="time" value={formdata.start} onChange={handlechange} className='border w-full rounded-md bg-gray-700 text-white p-1 outline-blue-400' id='start' />
-                    <label htmlFor="stop">To</label>
-                    <input type="time" value={formdata.end} onChange={handlechange} className='border w-full rounded-md bg-gray-700 text-white p-1 outline-blue-400' id='stop' />
+                    <input type="time" required value={formdata.start} onChange={handlechange} className='border w-full rounded-md bg-gray-700 text-white p-1 outline-blue-400' id='start' />
+                    <label htmlFor="end">To</label>
+                    <input type="time" value={formdata.end} onChange={handlechange} className='border w-full rounded-md bg-gray-700 text-white p-1 outline-blue-400' id='end' />
                 </div>
                 <div className=''>
                     <label htmlFor="date">Date</label>
-                    <input type="date" id='date' value={formdata.date} onChange={handlechange} className='border w-full rounded-md bg-gray-700 text-white p-1 outline-blue-400' />
+                    <input type="date" id='date' required value={formdata.date} onChange={handlechange} className='border w-full rounded-md bg-gray-700 text-white p-1 outline-blue-400' />
                 </div>
                 <div className=''>
                     <label htmlFor="note">Notes</label>
                     <textarea name="note" id="note" value={formdata.note} onChange={handlechange} placeholder='Add any details...' className='border w-full rounded-md bg-gray-700 text-white p-1 min-h-30 outline-blue-400' ></textarea>
                 </div>
                 <div className=' pb-2 flex justify-end gap-5'>
-                    <button onClick={()=>{setIsFormOpen(false)}} className='cursor-pointer'>Cancel</button>
-                    <button type='submit' className='bg-gradient-to-br from-blue-600 to-indigo-900 p-2 rounded-md text-white cursor-pointer shadow-md'>Save Task</button>
+                    <button onClick={()=>{setIsFormOpen(false)}} className='cursor-pointer transition-all ease-in-out hover:bg-gray-400 rounded-md p-2'>Cancel</button>
+                    <button type='submit' className='bg-blue-600 p-2 rounded-md text-white cursor-pointer shadow-md hover:bg-blue-900 transition-all ease-in-out'>Save Task</button>
                 </div>
             </form>
         </div>
