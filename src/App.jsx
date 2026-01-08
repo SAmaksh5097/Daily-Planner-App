81ca6320bdc918d283aa4a951452be0a61201f23
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-01-09 00:38:17 +0530

    browser route added and made functionable

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import AnalyticsPage from "./AnalyticsPage"
 import Dashboard from "./Dashboard"
 import Header from "./Header"
+import {Route, Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="min-h-screen max-h-full flex flex-col transition-all ease-in-out bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-50">
       <Header/>
-      <Dashboard/>
-      <AnalyticsPage/>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}/>
+        <Route path='/analytics' element={<AnalyticsPage/>}/>
+      </Routes>
     </div>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import ThemeToggle from './Components/ThemeToggle'
 import {CalendarCheck2} from 'lucide-react'
 import MenuButton from './Components/MenuButton'
+import { Link } from 'react-router-dom'
 const Header = () => {
   return (
+    
     <div className='h-fit w-full flex flex-row justify-between items-center p-3  bg-white/80 dark:bg-slate-900/80 backdrop-blur-md border-b border-slate-200 dark:border-slate-800 shadow-sm shadow-slate-200/50'>
+      <Link to={'/'}>
         <h1 className='text-2xl font-bold flex gap-2 items-center '>{<CalendarCheck2 className=' rounded w-10 h-10 text-blue-600 dark:text-blue-400'/>} Daily Planner</h1>
+      </Link>
         <div className='flex gap-10'>
-          <MenuButton name="Dashboard"/>
-        <MenuButton name="Analytics"/>
+          <Link to={'/'}>
+            <MenuButton name="Dashboard"/>
+          </Link>
+          <Link to={'/analytics'}>
+            <MenuButton name="Analytics"/>
+          </Link>
         </div>
         <ThemeToggle/>
     </div>
