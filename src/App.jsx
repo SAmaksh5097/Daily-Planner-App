b6d1b9cea365402049c0a2e269dfe5e4ec157f5e
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-01-09 00:13:36 +0530

    dashboard page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import AnalyticsPage from "./AnalyticsPage"
+import Dashboard from "./Dashboard"
 import Header from "./Header"
-import PlannerGrid from "./PlannerGrid"
-import QuickActions from "./QuickActions"
-import SideBar from "./SideBar"
 function App() {
   return (
     <div className="min-h-screen max-h-full flex flex-col transition-all ease-in-out bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-50">
       <Header/>
-      <div className="flex flex-col lg:flex-row flex-1 overflow-x-hidden items-stretch bg-transparent dark:bg-gray-600 w-full ">
-        <SideBar/>
-        <PlannerGrid/>
-        <QuickActions/>
-      </div>
+      <Dashboard/>
       <AnalyticsPage/>
     </div>
   )
