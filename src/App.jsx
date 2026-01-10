3196fbc11ccd5d95cd7e7f7150014a8e0df87ac3
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-01-10 23:03:28 +0530

    bit changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import AnalyticsPage from "./AnalyticsPage"
 import Dashboard from "./Dashboard"
 import Header from "./Header"
 import {Route, Routes} from 'react-router-dom'
-import {motion, AnimatePresence} from 'framer-motion'
 function App() {
   return (
-      <div layout initial={{opacity:0,y:20}} animate={{opacity:1, y:0,transition: { duration: 0.5 }}} className="min-h-screen max-h-full flex flex-col transition-all ease-in-out bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-50">
+      <div className="min-h-screen max-h-full flex flex-col transition-all ease-in-out bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-50">
         <Header/>
         <Routes>
           <Route path='/' element={<Dashboard/>}/>
