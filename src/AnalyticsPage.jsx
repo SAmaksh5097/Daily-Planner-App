b3ae0994186ade3b491c6ddc64da19b6a6b780cc
SAmaksh5097 <samaksharzare.cs2004@gmail.com>
2026-01-10 23:00:13 +0530

    analytics page's header added & animations added in tasks card

--- src/AnalyticsPage.jsx
+++ src/AnalyticsPage.jsx
@@ -1,11 +1,39 @@
-import React, { useState } from 'react'
+import React, { useContext, useMemo, useState } from 'react'
+import { PlannerContext } from './Context/PlannerContext'
+import APHeader from './Components/APHeader'
+import MetricCard from './Components/MetricCard'
+import { motion, AnimatePresence } from 'framer-motion'
 
 const AnalyticsPage = () => {
   
+  const {tasks} = useContext(PlannerContext)
+  const [startDate, setStartDate] = useState(()=>{
+    const d = new Date()
+    d.setDate(d.getDate()-7)
+    return d.toISOString().split('T')[0]
+  })
+  const [endDate,setEndDate] = useState(new Date().toISOString().split('T')[0])
+
+  const filtertask = useMemo(()=>{
+    return tasks.filter(task=>{
+      return task.date>=startDate && task.date<=endDate
+    },[tasks,startDate,endDate])
+  })
+
+  const total = filtertask.length;
+  const completed = filtertask.filter(task=>task.status==="completed").length;
     
   return (
-    <div>
-        <h1>Productivity Analytics</h1>
+    <div className='w-full h-screen px-4 py-2 flex flex-col gap-4'>
+      <APHeader startDate={startDate} endDate={endDate} setEndDate={setEndDate} setStartDate={setStartDate}/>
+      <div className='flex justify-evenly gap-4'>
+        <AnimatePresence mode='popLayout'>
+          <MetricCard name="Total Tasks" total = {total} completed={completed} heading="tasks completed"/>
+          <MetricCard name="Completion Rate" total = {total} completed={completed} heading="%"/>
+        </AnimatePresence>
+
+      </div>
+
         
     </div>
   )
--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import AnalyticsPage from "./AnalyticsPage"
 import Dashboard from "./Dashboard"
 import Header from "./Header"
 import {Route, Routes} from 'react-router-dom'
+import {motion, AnimatePresence} from 'framer-motion'
 function App() {
   return (
-    <div className="min-h-screen max-h-full flex flex-col transition-all ease-in-out bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-50">
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Dashboard/>}/>
-        <Route path='/analytics' element={<AnalyticsPage/>}/>
-      </Routes>
-    </div>
+      <div layout initial={{opacity:0,y:20}} animate={{opacity:1, y:0,transition: { duration: 0.5 }}} className="min-h-screen max-h-full flex flex-col transition-all ease-in-out bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-50">
+        <Header/>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+          <Route path='/analytics' element={<AnalyticsPage/>}/>
+        </Routes>
+      </div>
   )
 }
 
